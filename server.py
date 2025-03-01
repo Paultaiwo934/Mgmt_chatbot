6fc19a2737baf4671894d98b0d32f2f6f78d8df3
Kehinde Onyekuwuluje <kehindepeter79@gmail.com>
2025-03-01 19:15:36 +0100

    Update

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import openai
+import os
 
 app = Flask(__name__)
 
@@ -37,4 +38,5 @@ def ask():
     return response_data
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT isn't set
+    app.run(host="0.0.0.0", port=port)
